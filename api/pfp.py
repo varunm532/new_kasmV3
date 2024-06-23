842ff1ee5687661ed38a0d7ae6b8c10a5153db7f
jm1021 <jmortensen@powayusd.com>
2024-06-23 07:43:03 -0700

    restructure pfp.py get to work on logged in ID

--- api/pfp.py
+++ api/pfp.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, request, current_app as app
+from flask import Blueprint, g, request, current_app as app
 from flask_restful import Api, Resource
 import base64
+from api.auth_middleware import token_required
 from model.users import User
 from __init__ import db
 import os
 from werkzeug.utils import secure_filename
 
-pfp_api = Blueprint('pfp_api', __name__, url_prefix='/api/user')
+pfp_api = Blueprint('pfp_api', __name__, url_prefix='/api/id')
 api = Api(pfp_api)
 
 def allowed_file(filename):
@@ -49,17 +50,13 @@ class _PFP(Resource):
             db.session.rollback()
             return {'message': f'An error occurred while updating the profile picture: {str(e)}'}, 500
 
+    @token_required()
     def get(self):
-        user_uid = request.args.get('uid')
-        if not user_uid:
-            return {'message': 'UID required.'}, 400
+        # retrieve the current user from the token_required authentication check  
+        current_user = g.current_user
 
-        user = User.query.filter_by(_uid=user_uid).first()
-        if not user:
-            return {'message': 'User not found'}, 404
-
-        if user._pfp:
-            img_path = os.path.join(app.config['UPLOAD_FOLDER'], user._pfp)
+        if current_user._pfp:
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], current_user.uid, current_user.pfp)
             try:
                 with open(img_path, 'rb') as img_file:
                     base64_encoded = base64.b64encode(img_file.read()).decode('utf-8')
--- api/user.py
+++ api/user.py
@@ -2,7 +2,7 @@ import json, jwt
 from flask import Blueprint, request, jsonify, current_app, Response, g
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
-from auth_middleware import token_required
+from api.auth_middleware import token_required
 
 from model.users import User
 
