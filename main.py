a07956466cfed254cce9b884c890bfbe48904b26
rachit-j <rjaiswal.sd.77@gmail.com>
2024-07-04 16:55:54 -0700

    change docker port

--- main.py
+++ main.py
@@ -128,4 +128,4 @@ app.cli.add_command(custom_cli)
 # this runs the flask application on the development server
 if __name__ == "__main__":
     # change name for testing
-    app.run(debug=True, host="0.0.0.0", port="8086")
+    app.run(debug=True, host="0.0.0.0", port="8087")
