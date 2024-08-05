d3fbfa5d5a15bd4841008136f4872e78055b5448
tuckergol <golparianitucker@gmail.com>
2024-08-05 12:34:54 -0700

    implement GitHub user information API requests

--- api/analytics.py
+++ api/analytics.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
-from model.github import GitHubUser
+from model.github import GitHubUser, GitHubOrg
 
 analytics_api = Blueprint('analytics_api', __name__, url_prefix='/api/analytics')
 api = Api(analytics_api)
@@ -18,7 +18,68 @@ class GitHubUserAPI(Resource):
         except Exception as e:
             return {'message': str(e)}, 500
 
+class UserProfileLinks(Resource):
+    def get(self, uid):
+        try:
+            github_user_resource = GitHubUser()
+            response = github_user_resource.get_profile_links(uid)
+            
+            if response[1] != 200:
+                return response
+
+            return jsonify(response[0])
+        except Exception as e:
+            return {'message': str(e)}, 500
+
+class UserCommits(Resource):
+    def get(self, uid):
+        try:
+            start_date = request.args.get('start_date')
+            end_date = request.args.get('end_date')
+            if not start_date or not end_date:
+                return {'message': 'start_date and end_date parameters are required'}, 400
+
+            github_user_resource = GitHubUser()
+            response = github_user_resource.get_commit_stats(uid, start_date, end_date)
+            
+            if response[1] != 200:
+                return response
+
+            return jsonify(response[0])
+        except Exception as e:
+            return {'message': str(e)}, 500
+
+class GitHubOrgUsers(Resource):
+    def get(self, org_name):
+        try:
+            github_org_resource = GitHubOrg()
+            response = github_org_resource.get_users(org_name)
+            
+            if response[1] != 200:
+                return response
+
+            return jsonify(response[0])
+        except Exception as e:
+            return {'message': str(e)}, 500
+
+class GitHubOrgRepos(Resource):
+    def get(self, org_name):
+        try:
+            github_org_resource = GitHubOrg()
+            response = github_org_resource.get_repos(org_name)
+            
+            if response[1] != 200:
+                return response
+
+            return jsonify(response[0])
+        except Exception as e:
+            return {'message': str(e)}, 500
+
 api.add_resource(GitHubUserAPI, '/github/user/<string:uid>')
+api.add_resource(UserProfileLinks, '/github/user/<string:uid>/profile_links')
+api.add_resource(UserCommits, '/github/user/<string:uid>/commits')
+api.add_resource(GitHubOrgUsers, '/github/org/<string:org_name>/users')
+api.add_resource(GitHubOrgRepos, '/github/org/<string:org_name>/repos')
 
 if __name__ == '__main__':
     from __init__ import app
--- model/github.py
+++ model/github.py
@@ -9,7 +9,6 @@ class GitHubUser(Resource):
         if not token:
             return {'message': 'GITHUB_TOKEN not set'}, 400
 
-        # Validate UID and get user data from GitHub API
         try:
             headers = {'Authorization': f'token {token}'}
             response = requests.get(url, headers=headers)
@@ -18,6 +17,89 @@ class GitHubUser(Resource):
                 return {'message': f'Invalid UID {uid}'}, 404
             elif response.status_code != 200:
                 return {'message': 'GitHub API failed to fetch user data'}, response.status_code
+
+            return response.json(), 200
+
+        except Exception as e:
+            return {'message': str(e)}, 500
+    
+    def get_profile_links(self, uid):
+        user_data, status_code = self.get(uid)
+        if status_code != 200:
+            return user_data, status_code
+        
+        profile_links = {
+            'profile_url': user_data.get('html_url'),
+            'repos_url': user_data.get('repos_url')
+        }
+        return profile_links, 200
+
+    def get_commit_stats(self, uid, start_date, end_date):
+        url = app.config['GITHUB_API_GRAPHQL_URL']
+        token = app.config['GITHUB_TOKEN']
+        if not token:
+            return {'message': 'GITHUB_TOKEN not set'}, 400
+
+        query = """
+        query($login: String!, $from: DateTime!, $to: DateTime!) {
+            user(login: $login) {
+                contributionsCollection(from: $from, to: $to) {
+                    totalCommitContributions
+                }
+            }
+        }
+        """
+        variables = {
+            "login": uid,
+            "from": start_date,
+            "to": end_date
+        }
+        try:
+            headers = {'Authorization': f'bearer {token}'}
+            response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers)
+
+            if response.status_code != 200:
+                return {'message': 'GitHub API failed to fetch commit stats'}, response.status_code
+
+            data = response.json()
+            commit_stats = data['data']['user']['contributionsCollection']['totalCommitContributions']
+            return {'total_commit_contributions': commit_stats}, 200
+
+        except Exception as e:
+            return {'message': str(e)}, 500
+
+
+class GitHubOrg(Resource):
+    def get_users(self, org_name):
+        url = app.config['GITHUB_API_URL'] + f'/orgs/{org_name}/members'
+        token = app.config['GITHUB_TOKEN']
+        if not token:
+            return {'message': 'GITHUB_TOKEN not set'}, 400
+
+        try:
+            headers = {'Authorization': f'token {token}'}
+            response = requests.get(url, headers=headers)
+
+            if response.status_code != 200:
+                return {'message': 'GitHub API failed to fetch organization members'}, response.status_code
+
+            return response.json(), 200
+
+        except Exception as e:
+            return {'message': str(e)}, 500
+
+    def get_repos(self, org_name):
+        url = app.config['GITHUB_API_URL'] + f'/orgs/{org_name}/repos'
+        token = app.config['GITHUB_TOKEN']
+        if not token:
+            return {'message': 'GITHUB_TOKEN not set'}, 400
+
+        try:
+            headers = {'Authorization': f'token {token}'}
+            response = requests.get(url, headers=headers)
+
+            if response.status_code != 200:
+                return {'message': 'GitHub API failed to fetch organization repositories'}, response.status_code
 
             return response.json(), 200
 
