00f5a1977db2eeaca02f081d4601ee8fe820e4d8
tuckergol <golparianitucker@gmail.com>
2024-08-07 12:03:23 -0700

    implement current_user for analytics

--- api/analytics.py
+++ api/analytics.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, g
 from flask_restful import Api, Resource
+from api.jwt_authorize import token_required
 from model.github import GitHubUser, GitHubOrg
 
 analytics_api = Blueprint('analytics_api', __name__, url_prefix='/api/analytics')
 api = Api(analytics_api)
 
 class GitHubUserAPI(Resource):
-    def get(self, uid):
+    @token_required()
+    def get(self):
         try:
+            current_user = g.current_user
             github_user_resource = GitHubUser()
-            response = github_user_resource.get(uid)
+            response = github_user_resource.get(current_user.uid)
             
             if response[1] != 200:
                 return response
@@ -19,10 +22,12 @@ class GitHubUserAPI(Resource):
             return {'message': str(e)}, 500
 
 class UserProfileLinks(Resource):
-    def get(self, uid):
+    @token_required()
+    def get(self):
         try:
+            current_user = g.current_user
             github_user_resource = GitHubUser()
-            response = github_user_resource.get_profile_links(uid)
+            response = github_user_resource.get_profile_links(current_user.uid)
             
             if response[1] != 200:
                 return response
@@ -32,8 +37,10 @@ class UserProfileLinks(Resource):
             return {'message': str(e)}, 500
 
 class UserCommits(Resource):
-    def get(self, uid):
+    @token_required()
+    def get(self):
         try:
+            current_user = g.current_user
             body = request.get_json()
             start_date = body.get('start_date')
             end_date = body.get('end_date')
@@ -41,7 +48,7 @@ class UserCommits(Resource):
                 return {'message': 'start_date and end_date parameters are required'}, 400
 
             github_user_resource = GitHubUser()
-            response = github_user_resource.get_commit_stats(uid, start_date, end_date)
+            response = github_user_resource.get_commit_stats(current_user.uid, start_date, end_date)
             
             if response[1] != 200:
                 return response
@@ -51,6 +58,7 @@ class UserCommits(Resource):
             return {'message': str(e)}, 500
 
 class GitHubOrgUsers(Resource):
+    @token_required()
     def get(self, org_name):
         try:
             github_org_resource = GitHubOrg()
@@ -64,6 +72,7 @@ class GitHubOrgUsers(Resource):
             return {'message': str(e)}, 500
 
 class GitHubOrgRepos(Resource):
+    @token_required()
     def get(self, org_name):
         try:
             github_org_resource = GitHubOrg()
@@ -76,13 +85,8 @@ class GitHubOrgRepos(Resource):
         except Exception as e:
             return {'message': str(e)}, 500
 
-api.add_resource(GitHubUserAPI, '/github/user/<string:uid>')
-api.add_resource(UserProfileLinks, '/github/user/<string:uid>/profile_links')
-api.add_resource(UserCommits, '/github/user/<string:uid>/commits')
+api.add_resource(GitHubUserAPI, '/github/user')
+api.add_resource(UserProfileLinks, '/github/user/profile_links')
+api.add_resource(UserCommits, '/github/user/commits')
 api.add_resource(GitHubOrgUsers, '/github/org/<string:org_name>/users')
 api.add_resource(GitHubOrgRepos, '/github/org/<string:org_name>/repos')
-
-if __name__ == '__main__':
-    from __init__ import app
-    app.register_blueprint(analytics_api)
-    app.run(debug=True)
