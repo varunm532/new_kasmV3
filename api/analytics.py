581476147adec02ffcfe5a5dc3c8dc0df8229b32
jm1021 <jmortensen@powayusd.com>
2024-08-07 13:06:14 -0700

    adjust user to work on @token_required and default dates for commits.

--- api/analytics.py
+++ api/analytics.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, g
 from flask_restful import Api, Resource
+from datetime import datetime
 from api.jwt_authorize import token_required
 from model.github import GitHubUser, GitHubOrg
 
@@ -41,11 +42,37 @@ class UserCommits(Resource):
     def get(self):
         try:
             current_user = g.current_user
-            body = request.get_json()
+            try:
+                body = request.get_json()
+            except Exception as e:
+                body = {}
+            
             start_date = body.get('start_date')
             end_date = body.get('end_date')
+            
+            # If start_date or end_date is not provided, determine the current trimester
             if not start_date or not end_date:
-                return {'message': 'start_date and end_date parameters are required'}, 400
+                today = datetime.today()
+                year = today.year
+
+                # Tri 1: Aug 15 to Nov 14 (includes June 15 to Aug 14 of the previous year) 
+                if today >= datetime(year, 6, 15) and today <= datetime(year, 11, 14):
+                    start_date = datetime(year, 6, 15)
+                    end_date = datetime(year, 11, 14)
+                # Tri 2: Nov 15 to March 14
+                elif today >= datetime(year, 11, 15) and today <= datetime(year + 1, 3, 14):
+                    start_date = datetime(year, 11, 15)
+                    end_date = datetime(year + 1, 3, 14)
+                # Tri 3: April 15 to June 14
+                elif today >= datetime(year, 4, 15) and today <= datetime(year, 6, 14):
+                    start_date = datetime(year, 4, 15)
+                    end_date = datetime(year, 6, 14)
+                else:
+                    return {'message': 'Date is out of the defined trimesters'}, 400
+
+                # Convert dates to ISO 8601 format
+                start_date = start_date.strftime('%Y-%m-%d')
+                end_date = end_date.strftime('%Y-%m-%d')
 
             github_user_resource = GitHubUser()
             response = github_user_resource.get_commit_stats(current_user.uid, start_date, end_date)
@@ -58,7 +85,6 @@ class UserCommits(Resource):
             return {'message': str(e)}, 500
 
 class GitHubOrgUsers(Resource):
-    @token_required()
     def get(self, org_name):
         try:
             github_org_resource = GitHubOrg()
@@ -72,7 +98,6 @@ class GitHubOrgUsers(Resource):
             return {'message': str(e)}, 500
 
 class GitHubOrgRepos(Resource):
-    @token_required()
     def get(self, org_name):
         try:
             github_org_resource = GitHubOrg()
