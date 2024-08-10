033d2636c25f784732e3f753769ab1760c105918
jm1021 <jmortensen@powayusd.com>
2024-08-10 05:46:46 -0700

    simple migrate schema with  test users only

--- scripts/db_init.py
+++ scripts/db_init.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" db_init.py
+""" db_migrate.py
 Generates the database schema for all db models
 - Initializes Users, Sections, and UserSections tables.
 - Imports data from the old database to the new database.
@@ -8,29 +8,27 @@ Generates the database schema for all db models
 Usage: Run from the terminal as such:
 
 Goto the scripts directory:
-> cd scriipts; ./db_init.py
+> cd scripts; ./db_migrate.py
 
 Or run from the root of the project:
-> scripts/db_init.py
+> scripts/db_migrate.py
 
-Process outline:
-
-1. New schema.  The schema is created in "this" new database.
-2. Old data extraction.  An API has been created in the old project ...
+General Process outline:
+0. Warning to the user.
+1. Old data extraction.  An API has been created in the old project ...
   - Extract Data: retrieves data from the specified tables in the old database.
   - Transform Data: the API to JSON format understood by the new project.
+2. New schema.  The schema is created in "this" new database.
 3. Load Data: The bulk load API in "this" project inserts the data using required business logic.
 
 """
 import shutil
 import sys
 import os
-import requests
 
 # Add the directory containing main.py to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-# Import the app and db objects
+# Import application object
 from main import app, db, initUsers
 
 # Backup the old database
@@ -44,49 +42,52 @@ def backup_database(db_uri, backup_uri):
     else:
         print("Backup not supported for production database.")
 
-# Obtain the app context 
-with app.app_context():
+# Main extraction and loading process
+def main():
+    
+    # Step 0: Warning to the user and backup table
+    with app.app_context():
+        try:
+            # Step 3: Build New schema
+            # Check if the database has any tables
+            inspector = db.inspect(db.engine)
+            tables = inspector.get_table_names()
+            
+            if tables:
+                print("Warning, you are about to lose all data in the database!")
+                print("Do you want to continue? (y/n)")
+                response = input()
+                if response.lower() != 'y':
+                    print("Exiting without making changes.")
+                    sys.exit(0)
+                    
+            # Backup the old database
+            backup_database(app.config['SQLALCHEMY_DATABASE_URI'], app.config['SQLALCHEMY_BACKUP_URI'])
+           
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
+        
+    # Step 1: Build New schema and create test data 
     try:
-        
-        # Step 1: New schema
-        # Check if the database has any tables
-        inspector = db.inspect(db.engine)
-        tables = inspector.get_table_names()
-        
-        if tables:
-            print("Warning, you are about to lose all data in the database!")
-            print("Do you want to continue? (y/n)")
-            response = input()
-            if response.lower() != 'y':
-                print("Exiting without making changes.")
-                sys.exit(0)
-        
-        # Backup the old database
-        backup_database(app.config['SQLALCHEMY_DATABASE_URI'], app.config['SQLALCHEMY_BACKUP_URI'])
-        
-        # Drop all tables defined in the project
-        db.drop_all()
-        print("All tables dropped.")
-        
-        # Create all tables
-        db.create_all()
-        print("All tables created.")
-        
-        # Add default test data 
-        initUsers() # test data
-        
-        # Step 3: Load data into the new database using Flask's test client
-        with app.test_client() as client:
-            post_response = client.post('/api/users', json=old_data)
-            if post_response.status_code == 200:
-                print("Data loaded into the new database successfully.")
-            else:
-                print(f"Failed to load data into the new database. Status code: {post_response.status_code}")
-                sys.exit(1)
-        
+        with app.app_context():
+            # Drop all the tables defined in the project
+            db.drop_all()
+            print("All tables dropped.")
+            
+            # Create all tables
+            db.create_all()
+            print("All tables created.")
+            
+            # Add default test data 
+            initUsers() # test data
+            
     except Exception as e:
         print(f"An error occurred: {e}")
         sys.exit(1)
     
     # Log success 
     print("Database initialized!")
+ 
+if __name__ == "__main__":
+    main()
